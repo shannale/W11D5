70b7eca73cadfb971192109e2b9fbf6d68af774f
Shanna Le <shannatle@gmail.com>
2023-06-09 14:56:21 -0700

    Add hover effect

--- practice-for-sprint-15-react-redux-pet-rating-main/frontend/src/components/PawsRatingInput.js
+++ practice-for-sprint-15-react-redux-pet-rating-main/frontend/src/components/PawsRatingInput.js
@@ -1,7 +1,9 @@
+import {useState} from "react"
 const PawsRatingInput = ({ rating, disabled, onChange }) => {
+const [activeRating,setactiveRating] = useState("empty")
 
-  return (
-    <> 
+return (
+  <> 
     <input
       type="number"
       disabled={disabled}
@@ -10,20 +12,20 @@ const PawsRatingInput = ({ rating, disabled, onChange }) => {
     />
 
     <div className="rating-input">
-      <div className={`${rating >= 1 ? "filled" : "empty"}`}>
-        <i className="fa fa-paw"></i>
+      <div className={`${activeRating >= 1 ? "filled" : "empty"}`}>
+        <i className="fa fa-paw" onMouseEnter= {(e) => {setactiveRating(1)}}></i>
       </div>
-      <div className={`${rating >= 2 ? "filled" : "empty"}`}>
-        <i className="fa fa-paw"></i>
+      <div className={`${activeRating >= 2 ? "filled" : "empty"}`}>
+        <i className="fa fa-paw" onMouseEnter= {(e) => {setactiveRating(2)}}></i>
       </div>
-      <div className={`${rating >= 3 ? "filled" : "empty"}`}>
-        <i className="fa fa-paw"></i>
+      <div className={`${activeRating >= 3 ? "filled" : "empty"}`}>
+        <i className="fa fa-paw" onMouseEnter= {(e) => {setactiveRating(3)}}></i>
       </div>
-      <div className={`${rating >= 4 ? "filled" : "empty"}`}>
-        <i className="fa fa-paw"></i>
+      <div className={`${activeRating >= 4 ? "filled" : "empty"}`}>
+        <i className="fa fa-paw" onMouseEnter= {(e) => {setactiveRating(4)}}></i>
       </div>
-      <div className={`${rating >= 5 ? "filled" : "empty"}`}>
-        <i className="fa fa-paw"></i>
+      <div className={`${activeRating >= 5 ? "filled" : "empty"}`}>
+        <i className="fa fa-paw" onMouseEnter= {(e) => {setactiveRating(5)}}></i>
       </div>
     </div>
 
