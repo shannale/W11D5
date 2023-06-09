33002e01d3d55626a49904fa8cfb4d8c38008a37
Shanna Le <shannatle@gmail.com>
2023-06-09 14:03:28 -0700

    Change paw rating

--- practice-for-sprint-15-react-redux-pet-rating-main/frontend/src/components/PawsRatingInput.js
+++ practice-for-sprint-15-react-redux-pet-rating-main/frontend/src/components/PawsRatingInput.js
@@ -1,12 +1,34 @@
 const PawsRatingInput = ({ rating, disabled, onChange }) => {
 
   return (
+    <> 
     <input
       type="number"
       disabled={disabled}
       value={rating}
       onChange={onChange}
     />
+
+    <div className="rating-input">
+      <div className={`${rating >= 1 ? "filled" : "empty"}`}>
+        <i className="fa fa-paw"></i>
+      </div>
+      <div className={`${rating >= 2 ? "filled" : "empty"}`}>
+        <i className="fa fa-paw"></i>
+      </div>
+      <div className={`${rating >= 3 ? "filled" : "empty"}`}>
+        <i className="fa fa-paw"></i>
+      </div>
+      <div className={`${rating >= 4 ? "filled" : "empty"}`}>
+        <i className="fa fa-paw"></i>
+      </div>
+      <div className={`${rating >= 5 ? "filled" : "empty"}`}>
+        <i className="fa fa-paw"></i>
+      </div>
+    </div>
+
+    
+    </>
   );
 };
 
